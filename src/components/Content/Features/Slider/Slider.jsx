07c8d008018d9f2d508bf958bdd8e22614707c63
CopsLikeDonuts <reggisen@gmail.com>
2021-02-12 00:12:12 +0200

    almost completed slider

--- src/components/Content/Features/Slider/Slider.jsx
+++ src/components/Content/Features/Slider/Slider.jsx
@@ -1,40 +1,41 @@
 import React from "react";
 import './Slider.css';
-import { Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, {Pagination, Scrollbar} from 'swiper'; 
 
-import 'swiper/swiper-bundle.min.css';
-import 'swiper/swiper-bundle.min.js';
+// Import Swiper styles
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
+
+SwiperCore.use([Pagination, Scrollbar]);
 
 const Slider = () => {
-  const frames = ['slide1.png', 'slide2.png', 'slide3.png', 'slide4.png', 'slide5.png', 'slide6.png', 'slide7.png'];
-  const slides = frames.map(image => {
-    let src = './slide-frames/' + image;
-    return <div className='slide-frames'><img src={src}></img></div>
-  });
-  
   return (
-    <div className='slider-body'>
-      <div className="swiper-container">
-        <div className="swiper-wrapper">
-          <div className="swiper-slide">Slide 1</div>
-          <div className="swiper-slide">Slide 2</div>
-          <div className="swiper-slide">Slide 3</div>
-          <div className="swiper-slide">Slide 4</div>
-          <div className="swiper-slide">Slide 5</div>
-          <div className="swiper-slide">Slide 6</div>
-          <div className="swiper-slide">Slide 7</div>
-          <div className="swiper-slide">Slide 8</div>
-          <div className="swiper-slide">Slide 9</div>
-          <div className="swiper-slide">Slide 10</div>
-        </div>
-        <div className="swiper-pagination"></div>
-    </div>
+    <div>
+    <Swiper
+      spaceBetween={200}
+      slidesPerView={5}
+      // scrollbar={{ draggable: false, dragSize: 'auto' }}
+      pagination={{ clickable: true, type: 'custom'}}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      centeredSlides={true}
+    >
+      <SwiperSlide><img src='./slide-frames/slide1.png' className='frame'></img></SwiperSlide>
+      <SwiperSlide><img src='./slide-frames/slide2.png' className='frame'></img></SwiperSlide>
+      <SwiperSlide><img src='./slide-frames/slide3.png' className='frame'></img></SwiperSlide>
+      <SwiperSlide><img src='./slide-frames/slide4.png' className='frame'></img></SwiperSlide>
+      <SwiperSlide><img src='./slide-frames/slide5.png' className='frame'></img></SwiperSlide>
+      <SwiperSlide><img src='./slide-frames/slide6.png' className='frame'></img></SwiperSlide>
+      <SwiperSlide><img src='./slide-frames/slide7.png' className='frame'></img></SwiperSlide>
+
+    </Swiper>
+    
     <div className='test'></div>
-  </div>
-    
-    
+    </div>
   );
-  
 };
 
 export default Slider;
